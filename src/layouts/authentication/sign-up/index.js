580e33ea128e1207fa9a77244a003de1758bfdca
natali-alkayed <nataly2000alkayed@gmail.com>
2023-10-01 13:26:21 +0000

    start working with signup

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -1,35 +1,72 @@
 import { useState } from "react";
-
-// react-router-dom components
 import { Link } from "react-router-dom";
-
-// @mui material components
 import Card from "@mui/material/Card";
 import Checkbox from "@mui/material/Checkbox";
-
-// Soft UI Dashboard React components
 import SoftBox from "components/SoftBox";
 import SoftTypography from "components/SoftTypography";
 import SoftInput from "components/SoftInput";
 import SoftButton from "components/SoftButton";
-
-// Authentication layout components
 import BasicLayout from "layouts/authentication/components/BasicLayout";
 import Socials from "layouts/authentication/components/Socials";
 import Separator from "layouts/authentication/components/Separator";
-
-// Images
 import curved6 from "assets/images/curved-images/curved14.jpg";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
-  const [agreement, setAgremment] = useState(true);
+  const [agreement, setAgreement] = useState(true);
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+    phone: "",
+    location: "",
+    password: "",
+    role: "Provider", // Default role
+  });
+  const navigate = useNavigate();
 
-  const handleSetAgremment = () => setAgremment(!agreement);
+  const handleSetAgreement = () => setAgreement(!agreement);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("https://ev-rental.onrender.com/signup", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+
+      if (response.ok) {
+        navigate("/dashboard");
+      } else {
+        // Handle error here
+      }
+    } catch (error) {
+      console.error("Error during signup:", error);
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleRoleChange = (newRole) => {
+    setFormData({
+      ...formData,
+      role: newRole,
+    });
+  };
 
   return (
     <BasicLayout
       title="Welcome!"
-      description="Use these awesome forms to login or create new account in your project for free."
+      description="Use these awesome forms to sign up for your project for free."
       image={curved6}
     >
       <Card>
@@ -43,23 +80,56 @@ function SignUp() {
         </SoftBox>
         <Separator />
         <SoftBox pt={2} pb={3} px={3}>
-          <SoftBox component="form" role="form">
+          <SoftBox component="form" role="form" onSubmit={handleSubmit}>
             <SoftBox mb={2}>
-              <SoftInput placeholder="Name" />
+              <SoftInput
+                name="username"
+                placeholder="User Name"
+                value={formData.username}
+                onChange={handleInputChange}
+              />
             </SoftBox>
             <SoftBox mb={2}>
-              <SoftInput type="email" placeholder="Email" />
+              <SoftInput
+                name="email"
+                type="email"
+                placeholder="Email"
+                value={formData.email}
+                onChange={handleInputChange}
+              />
             </SoftBox>
             <SoftBox mb={2}>
-              <SoftInput type="password" placeholder="Password" />
+              <SoftInput
+                name="phone"
+                placeholder="Phone"
+                value={formData.phone}
+                onChange={handleInputChange}
+              />
+            </SoftBox>
+            <SoftBox mb={2}>
+              <SoftInput
+                name="location"
+                placeholder="Location"
+                value={formData.location}
+                onChange={handleInputChange}
+              />
+            </SoftBox>
+            <SoftBox mb={2}>
+              <SoftInput
+                name="password"
+                type="password"
+                placeholder="Password"
+                value={formData.password}
+                onChange={handleInputChange}
+              />
             </SoftBox>
             <SoftBox display="flex" alignItems="center">
-              <Checkbox checked={agreement} onChange={handleSetAgremment} />
+              <Checkbox checked={agreement} onChange={handleSetAgreement} />
               <SoftTypography
                 variant="button"
                 fontWeight="regular"
-                onClick={handleSetAgremment}
-                sx={{ cursor: "poiner", userSelect: "none" }}
+                onClick={handleSetAgreement}
+                sx={{ cursor: "pointer", userSelect: "none" }}
               >
                 &nbsp;&nbsp;I agree the&nbsp;
               </SoftTypography>
@@ -73,9 +143,46 @@ function SignUp() {
                 Terms and Conditions
               </SoftTypography>
             </SoftBox>
+            <SoftBox mb={2}>
+              <SoftBox>
+                <SoftTypography
+                  variant="button"
+                  fontWeight="bold"
+                  sx={{
+                    cursor: "pointer",
+                    userSelect: "none",
+                    backgroundColor:
+                      formData.role === "Provider" ? "primary.main" : "transparent",
+                    color: formData.role === "Provider" ? "#fff" : "inherit",
+                    marginRight: "16px",
+                    padding: "8px 16px",
+                    borderRadius: "4px",
+                  }}
+                  onClick={() => handleRoleChange("Provider")}
+                >
+                  Provider
+                </SoftTypography>
+                <SoftTypography
+                  variant="button"
+                  fontWeight="bold"
+                  sx={{
+                    cursor: "pointer",
+                    userSelect: "none",
+                    backgroundColor:
+                      formData.role === "Renter" ? "primary.main" : "transparent",
+                    color: formData.role === "Renter" ? "#fff" : "inherit",
+                    padding: "8px 16px",
+                    borderRadius: "4px",
+                  }}
+                  onClick={() => handleRoleChange("Renter")}
+                >
+                  Renter
+                </SoftTypography>
+              </SoftBox>
+            </SoftBox>
             <SoftBox mt={4} mb={1}>
-              <SoftButton variant="gradient" color="dark" fullWidth>
-                sign up
+              <SoftButton type="submit" variant="gradient" color="dark" fullWidth>
+                Sign Up
               </SoftButton>
             </SoftBox>
             <SoftBox mt={3} textAlign="center">
