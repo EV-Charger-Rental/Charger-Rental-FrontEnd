15bfaf68a78f724a463f220ee8c4c18ff9b473a9
ZainabMalkawi94 <zainab.m.malkawi@gmail.com>
2023-10-01 17:00:51 +0000

    Finished SignUp

--- src/layouts/authentication/sign-up/index.js
+++ src/layouts/authentication/sign-up/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import Card from "@mui/material/Card";
 import Checkbox from "@mui/material/Checkbox";
@@ -11,6 +11,7 @@ import Socials from "layouts/authentication/components/Socials";
 import Separator from "layouts/authentication/components/Separator";
 import curved6 from "assets/images/curved-images/curved14.jpg";
 import { useNavigate } from "react-router-dom";
+import { FormatIndentDecreaseSharp } from "@mui/icons-material";
 
 function SignUp() {
   const [agreement, setAgreement] = useState(true);
@@ -20,8 +21,11 @@ function SignUp() {
     phone: "",
     location: "",
     password: "",
-    role: "Provider", // Default role
+    role: "Renter", // Default role
+    chargerType: "", // New field for Charger Type
+    pricePerHour: "", // New field for Price per Hour
   });
+  const [providerOptionsVisible, setProviderOptionsVisible] = useState(false); // Track visibility of provider options
   const navigate = useNavigate();
 
   const handleSetAgreement = () => setAgreement(!agreement);
@@ -39,9 +43,32 @@ function SignUp() {
       });
 
       if (response.ok) {
+        // Successfully signed up, now make the provider call if the role is Provider
+        if (formData.role === "Provider") {
+          const providerResponse = await fetch("https://ev-rental.onrender.com/api/v2/charger", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+              type: formData.chargerType,
+              status: "available",
+              owner_id: 1, // You can set this to the user's ID if it's the same
+              price: formData.pricePerHour,
+              Chargerlocation: formData.location,
+            }),
+          });
+
+          if (providerResponse.ok) {
+            // Handle provider creation success
+          } else {
+            // Handle provider creation error
+          }
+        }
+
         navigate("/authentication/sign-in");
       } else {
-        // Handle error here
+        // Handle signup error
       }
     } catch (error) {
       console.error("Error during signup:", error);
@@ -61,6 +88,13 @@ function SignUp() {
       ...formData,
       role: newRole,
     });
+
+    // Show provider options when "Provider" is selected
+    if (newRole === "Provider") {
+      setProviderOptionsVisible(true);
+    } else {
+      setProviderOptionsVisible(false);
+    }
   };
 
   return (
@@ -124,27 +158,9 @@ function SignUp() {
               />
             </SoftBox>
 
+            {/* Role selection */}
             <SoftBox mb={4} display="flex" alignItems="center" justifyContent="center">
               <SoftBox>
-                <SoftTypography
-                  variant="button"
-                  fontWeight="bold"
-                  sx={{
-                    cursor: "pointer",
-                    userSelect: "none",
-                    backgroundColor:
-                      formData.role === "Provider" ? "primary.main" : "transparent",
-                    color: formData.role === "Provider" ? "#fff" : "inherit",
-                    marginRight: "auto",
-                    marginLeft: "auto",
-
-                    padding: "8px 16px",
-                    borderRadius: "4px",
-                  }}
-                  onClick={() => handleRoleChange("Provider")}
-                >
-                  Provider
-                </SoftTypography>
                 <SoftTypography
                   variant="button"
                   fontWeight="bold"
@@ -161,10 +177,82 @@ function SignUp() {
                 >
                   Renter
                 </SoftTypography>
+                <SoftTypography
+                  variant="button"
+                  fontWeight="bold"
+                  sx={{
+                    cursor: "pointer",
+                    userSelect: "none",
+                    backgroundColor:
+                      formData.role === "Provider" ? "primary.main" : "transparent",
+                    color: formData.role === "Provider" ? "#fff" : "inherit",
+                    marginRight: "auto",
+                    marginLeft: "auto",
+                    padding: "8px 16px",
+                    borderRadius: "4px",
+                  }}
+                  onClick={() => handleRoleChange("Provider")}
+                >
+                  Provider
+                </SoftTypography>
               </SoftBox>
             </SoftBox>
 
-            
+            {/* Provider options */}
+            {providerOptionsVisible && (
+              <>
+                <SoftBox mb={2}>
+                  <select
+                    name="chargerType"
+                    value={formData.chargerType}
+                    onChange={handleInputChange}
+                    style={{
+                      width: "100%",
+                      padding: "8px 12px",
+                      border: "1px solid #ccc",
+                      borderRadius: "4px",
+                      backgroundColor: "#fff",
+                      color: "#333",
+                      fontSize: "14px",
+                    }}
+                  >
+                    <option value="" disabled selected>
+                      Select Charger Type
+                    </option>
+                    <option value="Charger Type 1">Charger Type 1</option>
+                    <option value="Charger Type 2">Charger Type 2</option>
+                    <option value="Charger Type 3">Charger Type 3</option>
+                    <option value="Charger Type 4">Charger Type 4</option>
+                  </select>
+                </SoftBox>
+
+                <SoftBox mb={2}>
+                  <select
+                    name="pricePerHour"
+                    value={formData.pricePerHour}
+                    onChange={handleInputChange}
+                    style={{
+                      width: "100%",
+                      padding: "8px 12px",
+                      border: "1px solid #ccc",
+                      borderRadius: "4px",
+                      backgroundColor: "#fff",
+                      color: "#333",
+                      fontSize: "14px",
+                    }}
+                  >
+                    <option value="" disabled selected style={{ color: "#ccc" }}>
+                      Select Price per Hour
+                    </option>
+                    <option value="Price 1">$1 per hour</option>
+                    <option value="Price 2">$2 per hour</option>
+                    <option value="Price 3">$3 per hour</option>
+                    <option value="Price 4">$4 per hour</option>
+                  </select>
+                </SoftBox>
+              </>
+            )}
+
             <SoftBox display="flex" alignItems="center">
               <Checkbox checked={agreement} onChange={handleSetAgreement} />
               <SoftTypography
@@ -185,7 +273,7 @@ function SignUp() {
                 Terms and Conditions
               </SoftTypography>
             </SoftBox>
-            
+
             <SoftBox mt={4} mb={1}>
               <SoftButton type="submit" variant="gradient" color="dark" fullWidth>
                 Sign Up
