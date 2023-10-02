e3f8a451e99113b7c88fbdd5ab1bf2df6e0f8cc3
ZainabMalkawi94 <zainab.m.malkawi@gmail.com>
2023-10-02 10:17:50 +0000

    make a cards for chargers

--- src/examples/Navbars/DashboardNavbar/index.js
+++ src/examples/Navbars/DashboardNavbar/index.js
@@ -139,13 +139,41 @@ function DashboardNavbar({ absolute, light, isMini }) {
       </SoftBox>
       {isMini ? null : (
         <SoftBox sx={(theme) => navbarRow(theme, { isMini })}>
-          <SoftBox pr={1}>
-            <SoftInput
-              placeholder="Type here..."
-              icon={{ component: 'search', direction: 'left' }}
-            />
-          </SoftBox>
+         
           <SoftBox color={light ? 'white' : 'inherit'}>
+         
+              <IconButton
+                size="small"
+                color="inherit"
+                sx={navbarMobileMenu}
+                onClick={handleMiniSidenav}
+              >
+                <Icon className={light ? "text-white" : "text-dark"}>
+                  {miniSidenav ? "menu_open" : "menu"}
+                </Icon>
+              </IconButton>
+              <IconButton
+                size="small"
+                color="inherit"
+                sx={navbarIconButton}
+                onClick={handleConfiguratorOpen}
+              >
+                <Icon>settings</Icon>
+              </IconButton>
+              <IconButton
+                size="small"
+                color="inherit"
+                sx={navbarIconButton}
+                aria-controls="notification-menu"
+                aria-haspopup="true"
+                variant="contained"
+                onClick={handleOpenMenu}
+              >
+                <Icon className={light ? "text-white" : "text-dark"}>notifications</Icon>
+              </IconButton>
+              {renderMenu()}
+            </SoftBox>
+
             {loggedIn ? (
               <IconButton sx={navbarIconButton} size="small" onClick={handleLogout}>
                 <Icon
@@ -183,37 +211,6 @@ function DashboardNavbar({ absolute, light, isMini }) {
                 </IconButton>
               </Link>
             )}
-              <IconButton
-                size="small"
-                color="inherit"
-                sx={navbarMobileMenu}
-                onClick={handleMiniSidenav}
-              >
-                <Icon className={light ? "text-white" : "text-dark"}>
-                  {miniSidenav ? "menu_open" : "menu"}
-                </Icon>
-              </IconButton>
-              <IconButton
-                size="small"
-                color="inherit"
-                sx={navbarIconButton}
-                onClick={handleConfiguratorOpen}
-              >
-                <Icon>settings</Icon>
-              </IconButton>
-              <IconButton
-                size="small"
-                color="inherit"
-                sx={navbarIconButton}
-                aria-controls="notification-menu"
-                aria-haspopup="true"
-                variant="contained"
-                onClick={handleOpenMenu}
-              >
-                <Icon className={light ? "text-white" : "text-dark"}>notifications</Icon>
-              </IconButton>
-              {renderMenu()}
-            </SoftBox>
           </SoftBox>
         )}
       </Toolbar>
